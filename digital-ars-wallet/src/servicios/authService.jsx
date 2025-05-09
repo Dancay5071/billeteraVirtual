c386496436567876cc076ad0ae1f15984e15ee1d
Daniela Cabrera <96794484+Dancay5071@users.noreply.github.com>
2025-05-09 19:50:21 -0300

    Delete digital-ars-wallet directory
    archivo duplicado

--- digital-ars-wallet/src/servicios/authService.jsx
+++
@@ -1,7 +0,0 @@
-//authService.jsx
-
-import * as auth from '../api/local/auth.jsx';
-
-export const login = auth.login;
-export const logout = auth.logout;
-export const getUsuario = auth.getUsuario;
